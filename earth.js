bc6bc0301c1c3783d74f6e89ced56c1ec2600f05
AfsaraSilviAmi <afsarasilvi44@gmail.com>
2026-03-05 23:38:37 +0600

    did the plant section and toggle

--- earth.js
+++ earth.js
@@ -12,7 +12,7 @@ const displayCategory = (btns) =>{
     btns.forEach(btn => {
         const makeCatDiv = document.createElement("div");
         makeCatDiv.innerHTML = `
-         <button  onclick="loadPlant(${btn.id})" class="btn btn-success w-full justify-start mb-3">${btn.category_name}</button>
+         <button  id = "category-${btn.id}" onclick="loadPlant(${btn.id})" class="category-btn btn btn-ghost w-full justify-start mb-3">${btn.category_name}</button>
         `
          CatDiv.append(makeCatDiv);
     });
@@ -23,7 +23,10 @@ const loadPlant = (id) =>{
      const url = `https://openapi.programming-hero.com/api/category/${id}`;
     fetch(url)
     .then((res)=>res.json())
-    .then((data)=>displayPlant(data.plants));
+    .then((data)=>{
+      nonActiveBtn(`category-${id}`);
+      displayPlant(data.plants)
+    });
 }
 
 //display Plant
@@ -34,21 +37,21 @@ PlantDiv.innerHTML = "";
 plants.forEach(plant =>{
     const makePlantDiv = document.createElement("div");
     makePlantDiv.innerHTML = `
-      <div class="card bg-base-100 shadow-sm">
+      <div class="card bg-base-200 shadow-sm px-2 pt-2 w-full h-95">
   <figure>
     <img
       src="${plant.image}"
-      alt="${plant.name}" />
+      alt="${plant.name}"/>
   </figure>
-  <div class="card-body">
+  <div class="card-body pl-0 pr-0 pt-1 w-full"> 
     <h2 class="card-title">${plant.name}</h2>
-    <p class= "line-clamp-2">${plant.description}</p>
-    <div>
-    <div class="badge badge-soft badge-success">${plant.category}</div>
-    <p>${plant.price}</p>
+    <p class= "line-clamp-2 text-left">${plant.description}</p>
+    <div class="flex justify-between">
+    <div class="badge border border-green-500 bg-green-100 text-green-400 p-2">${plant.category}</div>
+    <p class="text-right font-semibold">৳${plant.price}</p>
     </div>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+    <div class="card-actions">
+      <button class="add-cart-btn btn btn-primary w-full mt-3 bg-green-800 rounded-2xl border-none">Add to Cart</button>
     </div>
   </div>
 </div>
@@ -58,4 +61,65 @@ plants.forEach(plant =>{
 
 }
 
+//load all Plants
+
+const loadAllPlant = () =>{
+  const url = "https://openapi.programming-hero.com/api/plants";
+  fetch(url)
+  .then((res)=>res.json())
+  .then((data)=>{
+    nonActiveBtn("all-tree-btn");
+    displayAllPlant(data.plants)
+  });
+}
+
+//display all Plants
+
+const displayAllPlant = (plants) =>{
+  let PlantDiv = document.getElementById("plant-div");
+PlantDiv.innerHTML = "";
+plants.forEach(plant =>{
+    const makePlantDiv = document.createElement("div");
+    makePlantDiv.innerHTML = `
+      <div class="card bg-base-200 shadow-sm px-2 pt-2 w-full h-95">
+  <figure>
+    <img
+      src="${plant.image}"
+      alt="${plant.name}"/>
+  </figure>
+  <div class="card-body pl-0 pr-0 pt-1 w-full"> 
+    <h2 class="card-title">${plant.name}</h2>
+    <p class= "line-clamp-2 text-left">${plant.description}</p>
+    <div class="flex justify-between">
+    <div class="badge border border-green-500 bg-green-100 text-green-400 p-2">${plant.category}</div>
+    <p class="text-right font-semibold">৳${plant.price}</p>
+    </div>
+    <div class="card-actions">
+      <button class="add-cart-btn btn btn-primary w-full mt-3 bg-green-800 rounded-2xl border-none">Add to Cart</button>
+    </div>
+  </div>
+</div>
+    `
+    PlantDiv.append(makePlantDiv);
+})
+}
+
+const nonActiveBtn = (id) =>{
+  const allBtn = document.querySelectorAll(".category-btn, #all-tree-btn");
+  allBtn.forEach(btn => {
+    btn.classList.remove("btn-success");
+    btn.classList.add("btn-ghost");
+  });
+  const activeBtn = document.getElementById(id);
+if(activeBtn){
+   activeBtn.classList.remove("btn-ghost");
+   activeBtn.classList.add("btn-success");
+}
+}
+
+const cartBtn = document.querySelectorAll(".add-cart-btn");
+
+
 loadCategory();
+loadAllPlant();
+
